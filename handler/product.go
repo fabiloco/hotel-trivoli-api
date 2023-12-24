b402fd894220fa14f45c91b8f6139278f352c550
Fabián Sánchez <faalsaru@gmail.com>
2023-12-23 20:46:17 -0500

    product module, model and handler

--- handler/product.go
+++ handler/product.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"fabiloco/hotel-trivoli-api/database"
 	"fabiloco/hotel-trivoli-api/model"
+	"fabiloco/hotel-trivoli-api/utils"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -20,14 +22,100 @@ func GetProducts(ctx *fiber.Ctx) error {
   result := database.DB.Find(&products)
 
   if result.Error != nil {
-		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"errorMessage": "Error trying retreive products from the database.",
-      "errors":  result.Error.Error(),
+
+    ctx.Locals("data", fiber.Map{
+			"errors": result.Error.Error(),
 		})
+    return ctx.SendStatus(fiber.StatusBadRequest)
+
+		// return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+		// 	"errorMessage": "Error trying retreive products from the database.",
+  //     "errors":  result.Error.Error(),
+		// })
   }
 
-  return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+  ctx.Locals("data", fiber.Map{
     "products": products,
-    "rowsAffected": result.RowsAffected,
   })
+
+  return ctx.SendStatus(fiber.StatusOK)
+}
+
+// ListProducts   godoc
+// @Summary       Create products
+// @Description   Create new products
+// @Tags          product
+// @Accept        json
+// @Param			    body  body  string  true  "Body of the request" SchemaExample({\n"name": "test product",\n"price": 2000,\n"stock": 100,\n"type": "test type"\n})
+// @Produce       json
+// @Success       200  {array}   model.Product
+// @Router        /product [post]
+func PostProducts(ctx *fiber.Ctx) error {
+  var body model.CreateProduct
+  
+  if err := ctx.BodyParser(&body); err != nil {
+    ctx.Locals("data", fiber.Map{
+			"errors": err.Error(),
+		})
+    return ctx.SendStatus(fiber.StatusBadRequest)
+  }
+
+  validationErrors := utils.ValidateInput(ctx, body)
+
+  if validationErrors != nil {
+    ctx.Locals("data", fiber.Map{
+			"errors": validationErrors,
+		})
+    return ctx.SendStatus(fiber.StatusBadRequest)
+  }
+
+  product := model.Product {
+    Name: body.Name,
+    Stock: body.Stock,
+    Price: body.Price,
+    Type: body.Type,
+  }
+
+  database.DB.Create(&product)
+
+  ctx.Locals("data", fiber.Map{
+    "product": body,
+  })
+
+  return ctx.SendStatus(fiber.StatusCreated)
+}
+
+
+func PutProduct(ctx *fiber.Ctx) error {
+  var body model.UpdateProduct
+  
+  if err := ctx.BodyParser(&body); err != nil {
+    ctx.Locals("data", fiber.Map{
+			"errors": err.Error(),
+		})
+    return ctx.SendStatus(fiber.StatusBadRequest)
+  }
+
+  validationErrors := utils.ValidateInput(ctx, body)
+
+  if validationErrors != nil {
+    ctx.Locals("data", fiber.Map{
+			"errors": validationErrors,
+		})
+    return ctx.SendStatus(fiber.StatusBadRequest)
+  }
+
+  product := model.Product {}
+
+  result := database.DB.First(&product, 2)
+
+  if result.Error != nil {
+
+  }
+
+  ctx.Locals("data", fiber.Map{
+    "product": product,
+  })
+
+  return ctx.SendStatus(fiber.StatusCreated)
 }
--- model/product.go
+++ model/product.go
@@ -2,11 +2,27 @@ package model
 
 import "gorm.io/gorm"
 
-// User struct
+// Product model info
+// @Description Products information in stock
 type Product struct {
 	gorm.Model
-	Name    string    `gorm:"not null" json:"name"`
-	Stock   uint8     `gorm:"not null" json:"stock"`
-	Type    string    `gorm:"not null" json:"type"`
-	Price   float32   `gorm:"not null" json:"price"`
+	Name    string    `gorm:"not null" json:"name"`   // product name
+	Stock   uint8     `gorm:"not null" json:"stock"`  // product stock avaliable
+	Type    string    `gorm:"not null" json:"type"`   // product type
+	Price   float32   `gorm:"not null" json:"price"`  // product price
+}
+
+type CreateProduct struct {
+  Name    string    `valid:"required,stringlength(3|100)"`
+  Stock   uint8     `valid:"required,numeric"`
+  Type    string    `valid:"required"`
+  Price   float32   `valid:"required,numeric"`
+}
+
+
+type UpdateProduct struct {
+  Name    string    `valid:"stringlength(3|100)"`
+  Stock   uint8     `valid:"numeric"`
+  Type    string    `valid:""`
+  Price   float32   `valid:"numeric"`
 }
